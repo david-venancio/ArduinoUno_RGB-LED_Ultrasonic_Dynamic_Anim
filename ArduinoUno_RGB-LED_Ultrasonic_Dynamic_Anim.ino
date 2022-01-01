695419988d97b35bc9770cb3307dfdfab23bf4f5
Louis David (de Campos) Venâncio <dv@3dlibre.com>
2022-01-01 17:51:03 +0000

    Add files via upload

---
+++ ArduinoUno_RGB-LED_Ultrasonic_Dynamic_Anim.ino
@@ -0,0 +1,68 @@
+// ArduinoUno_RGB-LED_Ultrasonic_Dynamic_Anim v0.0.1 
+// Code by Louis David Venancio 01.01.2022
+// meta_4@massmulti.org
+
+#include <Ultrasonic.h>
+#include <RGB_LED.h>
+
+#define LOG_TIMER_DELAY 100
+#define WHAT_IS_CLOSE   10  //closer than that = max speed anim
+#define WHAT_IS_FAR     50  //further than that = slowest speed anim
+
+RGB_LED LED(9,10,11);
+unsigned int speed = 5000;
+int log_timer_k=0;
+
+
+Ultrasonic ultrasonic(4,3); // (Trig PIN,Echo PIN)
+int last_range=0;
+
+void setup() 
+{ 
+  //LED.setFunction(StepRandom);
+  LED.setFunction(FadeRandom);
+  LED.setSpeed(speed);
+
+  Serial.begin(250000);
+  Serial.println("----------------------------------------");
+  Serial.println("- RGB-LED ANIMATION from distance");
+  Serial.println("  read from HR-SC04 Ultrasonic Ranging Module");
+  Serial.println("- Meta_4 2022.01"); 
+  Serial.println("- LED PWM pins R:9 G:10 B:11");
+  Serial.println("- HR-SC04 pins Trig:4 Echo:3");
+  Serial.println("- Hardware: Arduino UNO and compatibles");
+  Serial.println("- Init Complete. <3.");
+  Serial.println("-------------------------------- HELP --");
+  Serial.print("- messages sent every ");
+  Serial.print(LOG_TIMER_DELAY);
+  Serial.println(" loops :");
+  Serial.println("- distance in cm");
+  Serial.println("----------------------------------------");
+}
+
+void loop() 
+{
+  last_range = ultrasonic.Ranging(CM);
+  if(log_timer_k<LOG_TIMER_DELAY){
+    log_timer_k++;
+  }else{
+    log_timer_k=0;
+    Serial.print(last_range);
+    //using the the log_timer var to also time the sensor read = realtime answer+log.
+    //wanted : close = fast anim, distant or nothing = slow anim
+    if(last_range < WHAT_IS_CLOSE){
+      speed = 20000;
+    }
+    if(last_range > WHAT_IS_FAR){
+      speed = 100;
+    }
+    if(last_range > WHAT_IS_CLOSE && last_range < WHAT_IS_FAR){
+      speed = map(last_range, WHAT_IS_CLOSE, WHAT_IS_FAR, 20000, 100  );
+    }
+    Serial.print(" anim speed:");
+    Serial.println(speed);
+    LED.setSpeed(speed);
+    LED.delay(10);
+  }
+  LED.run();
+}
